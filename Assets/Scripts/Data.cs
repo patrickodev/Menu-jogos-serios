960e9d777845e86982f78cee5ab1f94d3dd4907f
Patrick Oliveira <patrickoliveirabrto@gmail.com>
2019-11-26 00:05:35 -0300

    Game Conditions

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    public static int VerificadorPesquisa = 0;
+    public static int Pesquisou = 0;
+
+    private GameObject[] Datas;
+
+    void Awake()
+    {
+        Datas = GameObject.FindGameObjectsWithTag("Data");
+        if(Datas.Length >= 2)
+        {
+            Destroy(Datas[0]);
+        }
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -19,16 +19,70 @@ public class SceneController : MonoBehaviour
 
     public void goToComputer()
     {
+        Data.VerificadorPesquisa++;
+        Debug.Log(Data.VerificadorPesquisa);
         SceneManager.LoadScene("Computer");
     }
 
     public void goToDoor()
     {
-        SceneManager.LoadScene("Door");
+        if (Data.Pesquisou > 0)
+        {
+            SceneManager.LoadScene("Door");
+        }
+        else
+        {
+            SceneManager.LoadScene("Door2");
+        }
     }
 
     public void goToUrna()
     {
         SceneManager.LoadScene("Urna");
     }
+
+    public void goToUrna1()
+    {
+        SceneManager.LoadScene("Urna 1");
+    }
+
+    public void goToUrna2()
+    {
+        SceneManager.LoadScene("Urna 2");
+    }
+
+    public void goToCandidato1()
+    {
+        Data.Pesquisou++;    
+        SceneManager.LoadScene("Candidato1");
+    }
+
+    public void goToCandidato2()
+    {
+        Data.Pesquisou++;
+        SceneManager.LoadScene("Candidato2");
+    }
+
+    public void goToObra()
+    {
+        Data.VerificadorPesquisa++;
+        Debug.Log(Data.VerificadorPesquisa);
+        SceneManager.LoadScene("ObraInacabada");
+    }
+
+    public void goToPosto()
+    {
+        Data.VerificadorPesquisa++;
+        Debug.Log(Data.VerificadorPesquisa);
+        SceneManager.LoadScene("Posto");
+    }
+
+    public void verificadorDeFinal()
+    {
+        if (Data.VerificadorPesquisa > 0)
+        {
+            
+        }
+    }
+
 }
